98f16a5307d6f44dbfe74ad5d0535a3bfdfff480
sebastian agudelo <sebasnino12@gmail.com>
2024-01-08 09:45:35 -0300

    organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/imagens/Banner.png" alt="Banner da app organo"/> 
+        </header>
+    )
+
+}
+
+export default Banner 
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,16 @@
+import './ListaSuspensa.css'
+const ListaSuspensa = (props) => {
+    return (
+    <div className='Lista-suspensa'>
+        <label>{props.label}</label>
+        <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+            <option value=""></option>
+            {props.itens.map(item => {
+                return<option key={item}>{item}</option>
+            })}
+        </select>
+    </div>
+    )
+}
+
+export default ListaSuspensa
